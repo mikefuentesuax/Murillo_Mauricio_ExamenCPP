ba30b6f7b7885136e9c1eb22904e93810193bbb1
mikefuentesuax <mmurimer@myuax.com>
2023-11-09 12:21:43 +0100

    Ejercicio_1

---
+++ Parte_3_1.cpp
@@ -0,0 +1,19 @@
+//
+// Created by mikef on 09/11/2023.
+//
+
+#include "Parte_3_1.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 1. Operaciones basicas.
+    int a, b, resultado = 0;
+    cout << "Ingrese el primer numero: "; cin >> a;
+    cout << "Ingrese el segundo numero: "; cin >> b;
+    resultado = a + b;
+    cout << "El resultado de la suma es: " << resultado << endl;
+
+    return 0;
+}
